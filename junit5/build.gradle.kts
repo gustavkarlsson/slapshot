ee14c0c9464f78a54eca2c9c7ab76082f83cf49f
Gustav Karlsson <gustav.karlsson@epidemicsound.com>
2024-12-23 11:30:00 +0100

    Upgrade stuff

--- junit5/build.gradle.kts
+++ junit5/build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.freeCompilerArgs += "-opt-in=se.gustavkarlsson.slapshot.core.InternalSlapshotApi"
+    compilerOptions.freeCompilerArgs.add("-opt-in=se.gustavkarlsson.slapshot.core.InternalSlapshotApi")
 }
 
 tasks.test {
